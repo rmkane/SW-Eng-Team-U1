923e3460dfa46181344be8d2ec634c6df03c3f00
SeanWeber <bluesmew@gmail.com>
2011-11-01 22:32:41 -0400

    Made log file a .txt

--- ProjectName/Common/src/Logger.java
+++ ProjectName/Common/src/Logger.java
@@ -9,17 +9,14 @@ public class Logger {
 
 	public void writeOut(String filename) {
 		try {
-			FileWriter outFile = new FileWriter(filename);
+			FileWriter outFile = new FileWriter(filename + ".txt");
 			PrintWriter out = new PrintWriter(outFile);
 
 			// Also could be written as follows on one line
 			// Printwriter out = new PrintWriter(new FileWriter(args[0]));
 
 			// Write text to file
-			out.println("This is line 1");
-			out.println("This is line 2");
-			out.print("This is line3 part 1, ");
-			out.println("this is line 3 part 2");
+			out.println("Hello World");
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
